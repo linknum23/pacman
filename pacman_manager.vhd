d3cc97eeb0ec5000a436d4c5ff917108bb6460a4
bryant <bryant@cea9dca5-4048-41bc-958f-9b301c446f7b>
2011-11-23 04:20:38 +0000

    git-svn-id: https://10.0.0.20/svn/projects/Pacman/pacman@687 cea9dca5-4048-41bc-958f-9b301c446f7b

---
+++ pacman_manager.vhd
@@ -0,0 +1,20 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use work.pacage.all;
+
+
+entity pacman_manager is
+   port(
+      clk : std_logic;
+      
+   );
+end pacman_manager;
+
+architecture Behavioral of pacman_manager is
+
+begin
+
+
+end Behavioral;
+
